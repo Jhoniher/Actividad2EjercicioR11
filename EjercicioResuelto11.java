b34fe4b17267f40272aa6ca06985ad5c9e5ebe38
Jhony Alpala <jhoniher95@gmail.com>
2023-09-07 21:03:28 -0500

    Ejercicio finalizado

--- EjercicioResuelto11.java
+++ EjercicioResuelto11.java
@@ -1,16 +1,17 @@
+//Mostrar el mayor de tres numeros diferentes
 import java.util.Scanner;
 public class EjercicioResuelto11 {
     public static void main(String[] args) {
         Scanner entrada=new Scanner(System.in);
         int a, b, c;
-        while(true){
+        while(true){ //Ciclo para repetir entradas en caso que ingrese numeros iguales
             System.out.println("Ingrese el primer numero: ");
             a=entrada.nextInt();
             System.out.println("Ingrese el segundo numero: ");
             b=entrada.nextInt();
             System.out.println("Ingrese el tercer numero: ");
             c=entrada.nextInt();
-            if(a==b || b==c || c==a){
+            if(a==b || b==c || c==a){ //Condicional de comparacion
                 System.out.println("Hay numeros iguales, ingrese numeros diferentes.");
             }
             else{
